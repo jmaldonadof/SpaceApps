1fee4dc01a9d54f867fc33cf1decd3f1673eafee
zeeerooo <boris.rq@gmail.com>
2017-04-29 21:42:17 -0300

    Vista para facts funcionando

--- spacejockey.py
+++ spacejockey.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route("/facts")
+def facts():
+    return render_template("facts.html")
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
